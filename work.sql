ff84e8e85551ab3153154b70395d3537f3adaca1
Azlan <muhammad.azlan@gfk.com>
2019-10-31 05:28:24 +0500

    edited work.sql file

--- work.sql
+++ work.sql
@@ -1,6 +1,6 @@
 SELECT p.productid,
  case when ps.name='SPR Special Order' then ps.name,
-case when ps.name='SPR Special Order' then ps.sku,
+case when ps.name='SPR Special Order' then ps.sku
 FROM product p 
 join productskus ps on (p.productid=ps.productid)
 
